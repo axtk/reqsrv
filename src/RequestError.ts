7a3cfe56de6de26c3c88808d302142b5092b0af3
axtk <axtkx@inbox.ru>
2024-11-13 18:50:56 +0300

    allow Error as RequestError param

--- src/RequestError.ts
+++ src/RequestError.ts
@@ -1,6 +1,10 @@
 export const DEFAULT_REQUEST_ERROR_NAME = 'RequestError';
 export const DEFAULT_REQUEST_ERROR_MESSAGE = 'Unspecified';
 
+function getProp<T>(x: object | void | undefined, prop: string): T | undefined {
+    return x && (prop in x) ? x[prop as keyof typeof x] as T : undefined;
+}
+
 export type RequestErrorParams = {
     name?: string;
     message?: string;
@@ -14,17 +18,17 @@ export class RequestError<T extends RequestErrorParams = {}> extends Error {
     status: RequestErrorParams['status'];
     statusText: RequestErrorParams['statusText'];
 
-    constructor(params: T | void) {
-        let statusMessage = [params?.status, params?.statusText]
-            .filter(Boolean)
-            .join(' ');
+    constructor(params: T | Error | void) {
+        let status = getProp(params, 'status');
+        let statusText = getProp(params, 'statusText');
+        let statusMessage = [status, statusText].filter(Boolean).join(' ');
 
         super(params?.message || statusMessage || DEFAULT_REQUEST_ERROR_MESSAGE);
         this.name = params?.name ?? DEFAULT_REQUEST_ERROR_NAME;
 
-        this.status = Number(params?.status ?? 0);
-        this.statusText = String(params?.statusText ?? '');
-        this.data = params?.data;
+        this.status = Number(status ?? 0);
+        this.statusText = String(statusText ?? '');
+        this.data = getProp(params, 'data');
 
         // @see https://github.com/Microsoft/TypeScript-wiki/blob/main/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
         Object.setPrototypeOf(this, RequestError.prototype);
