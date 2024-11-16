53dec97794a050ff54ca38ba055cf11e346d6fec
axtk <axtkx@inbox.ru>
2024-11-16 20:44:06 +0300

    remove request params and request query types

--- src/types/RequestParams.ts
+++
@@ -1,4 +0,0 @@
-import type {SchemaEntry} from './SchemaEntry';
-import type {PartialRequest} from './PartialRequest';
-
-export type RequestParams<T extends SchemaEntry> = PartialRequest<T, 'params'>;
--- src/types/RequestQuery.ts
+++
@@ -1,4 +0,0 @@
-import type {SchemaEntry} from './SchemaEntry';
-import type {PartialRequest} from './PartialRequest';
-
-export type RequestQuery<T extends SchemaEntry> = PartialRequest<T, 'query'>;
